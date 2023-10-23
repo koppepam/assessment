06a8eeb5ab79b64a4d17863af3762b86d09ee4e7
yoneyamarise <102212479+yoneyamarise@users.noreply.github.com>
2023-10-23 11:40:34 +0900

    Bootstrap で改良 (card を作った)

--- assessment.js
+++ assessment.js
@@ -14,14 +14,38 @@ assessmentButton.onclick = () => {
 
   // 診断表示エリア
   resultDivision.innerText = ''; //診断表示の重複削除
-  const header = document.createElement('h3');
-  header.innerText = '診断結果';
-  resultDivision.appendChild(header);
+  // const header = document.createElement('h3');
+  // header.innerText = '診断結果';
+  // resultDivision.appendChild(header);
+
+  // const paragraph = document.createElement('p');
+  // const result = assessment(userName);
+  // paragraph.innerText = result;
+  // resultDivision.appendChild(paragraph);
+
+  // Bootstrapで作成
+  // headerDivision
+  const headerDivision = document.createElement('div');
+  headerDivision.setAttribute('class', 'card-header text-bg-primary');
+  headerDivision.innerText = '診断結果';
+
+  // bodyDivision
+  const bodyDivision = document.createElement('div');
+  bodyDivision.setAttribute('class', 'card-body');
 
   const paragraph = document.createElement('p');
+  paragraph.setAttribute('class', 'card-text');
   const result = assessment(userName);
   paragraph.innerText = result;
-  resultDivision.appendChild(paragraph);
+  bodyDivision.appendChild(paragraph);
+
+  // resultDivision に Bootstrap のスタイルを適用する
+  resultDivision.setAttribute('class', 'card');
+
+  // headerDivision と bodyDivision を resultDivision に差し込む
+  resultDivision.appendChild(headerDivision);
+  resultDivision.appendChild(bodyDivision);
+
 
   // ツイートエリア
   tweetDivision.innerText = '';
